a81a92fcc16c5bb7d3d19d54c70f454dc4ab1d6e
DAAI093 <1107840447@qq.com>
2020-08-30 18:01:50 +0800

    feat: 新旧state&range merge函数存在问题

--- main.js
+++ main.js
@@ -1,31 +1,69 @@
-import { createElement, Component, render } from './tony-react'
+import { createElement, Component, render } from "./tony-react";
 
-class MyCoponent extends Component {
-    constructor() {
-        super()
-        this.state = {
-            a:1,
-            b:2
-        }
-    }
-    render() {
-        return <div>
-            MyCoponent
-            <h1>iayr {this.state.a.toString()}</h1>
-            {this.children}
-            <button onclick={() => {
-                this.setState({
-                    a: this.state.a + 1
-                })
-            }}>add</button>
-        </div>
-    }
+class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
+  }
 }
 
+class Board extends Component {
+  renderSquare(i) {
+    return <Square />;
+  }
 
+  render() {
+    const status = "Next player: X";
 
-render(<MyCoponent id="a" class="c">
-    <div>abc</div>
-    <div></div>
-    <div></div>
-</MyCoponent>, document.body)
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+render(<Game />, document.getElementById("root"));
