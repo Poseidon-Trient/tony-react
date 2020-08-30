b056edbbd5625349a6d52f49c7fc93f89d8df1fe
DAAI093 <1107840447@qq.com>
2020-08-30 16:39:54 +0800

    feat: 完善多种类型递归子组件渲染

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-for(let i of [1,2,3]) {
-    console.log(i);
+import { createElement, Component, render } from './tony-react'
+
+class MyCoponent extends Component {
+    render() {
+        return <div>
+            MyCoponent
+            <h1>iayr</h1>
+            {this.children}
+        </div>
+    }
 }
 
-function createElement(tagName, attributes, ...children) {
-    let e = document.createElement(tagName)
-    for (let p in attributes) {
-        e.setAttribute(p, attributes[p]);
-    }
 
-    for (let child of children) {
-        if (typeof child === 'string') {
-            child = document.createTextNode(child)
-        }
-        e.appendChild(child)
-    }
 
-    return e;
-}
-
-document.body.appendChild(<div id="a" class="c">
+render(<MyCoponent id="a" class="c">
     <div>abc</div>
     <div></div>
     <div></div>
-</div>)
+</MyCoponent>, document.body)
